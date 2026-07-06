b36fa18bb641ee1c6bd578a3290c4d5d07848c9e
[Saif al-Din] | Game Dev <tonmax100@gmail.com>
2026-07-06 16:19:59 +0200

    Replace messages table with users table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,6 @@
-DROP TABLE IF EXISTS messages;
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS users (
     id TEXT PRIMARY KEY,
-    to_email TEXT NOT NULL,
-    from_email TEXT NOT NULL,
-    subject TEXT,
-    body TEXT,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
     created_at INTEGER NOT NULL
 );
-CREATE INDEX IF NOT EXISTS idx_to_email ON messages(to_email);
